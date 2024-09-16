3c98591aef82799a7f9a5988bf09d6f5fbfe1351
Aleksandr Yarvinen <Aleksandr.Yarvinen@kmslh.com>
2024-09-16 21:41:06 +0200

    Step 0. Fetching of problem data

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import './style.css';
 
 let ctx = document.getElementById("chart").getContext("2d");
 let chart;
-let data = [10, 20, 30, 40, 50];
+let data = [];
+const dataLength = 10000; // Длина массива данных
+const largeData = Array.from({ length: dataLength }, (_, i) => i);
 
+// Функция для инициализации графика
 function initChart() {
-  // Если график уже существует, уничтожаем его перед созданием нового
   if (chart) {
     chart.destroy();
   }
@@ -43,62 +45,36 @@ function hideLoader() {
   document.getElementById("loader").style.display = "none";
 }
 
-// Обновление графика test
-function testUpdateChart(newData) {
-  console.log("testUpdateChart", newData);
-}
-
 // Обновление графика
 function updateChart(newData) {
   console.log("updateChartNewData", newData);
-  data = [...data, ...newData];
+  data.push(newData);
   chart.data.labels = data.map((_, i) => i + 1);
   chart.data.datasets[0].data = data;
   chart.update();
 }
 
-// Проблема 1: Блокировка основного потока
-function simulateLargeDataProcessing() {
+
+function initDataForChart() {
+   showLoader(); // Показываем лоадер при начале обработки
   const largeData = Array.from({ length: 1000 }, (_, i) => i);
-  
-  // largeData.forEach((value) => {
-  //   // Долгая обработка данных
-  //   for (let i = 0; i < 1000; i++) {}
-  //   updateChart([value]);
-  // });
-  
-  let currentIndex = 0;
 
-  showLoader(); // Показываем лоадер при начале обработки
 
-  function processChunk() {
-    const chunkSize = 1000; // Обрабатываем по 1000 элементов за раз
-    const newData = [];
-
-    for (let i = 0; i < chunkSize && currentIndex < largeData.length; i++) {
-      const value = largeData[currentIndex];
-      newData.push(value); // Собираем новые данные
-      currentIndex++;
-    }
-
-    // Обновляем график с новой порцией данных
-    updateChart(newData);
-
-    // Если есть еще данные, продолжаем обработку
-    if (currentIndex < largeData.length) {
-      setTimeout(processChunk, 0); // Отдаем управление браузеру и продолжаем позже
+  largeData.forEach((value) => {
+    if (value % 2 === 0) {
+      // Если число четное, обрабатываем с задержкой
+      setTimeout(() => {
+       updateChart(value);
+      }, Math.random() * 1000); // Случайная задержка до 1000 мс
     } else {
-      hideLoader(); // Скрываем лоадер после завершения обработки
+       updateChart(value);
     }
-  }
-
-  processChunk(); // Запускаем обработку
-  
+  });
+  hideLoader(); // Скрываем лоадер после завершения обработки
 }
 
 window.onload = () => {
   console.log('DOM fully loaded and parsed');
   initChart();
-  // Запуск проблемных функций
-  // simulateLargeDataProcessing(); // Проблема 1
+  initDataForChart();
 };
