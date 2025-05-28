367b94d777e2d5efc19f1b7280a57583b1181037
Aleksandr Yarvinen <Aleksandr.Yarvinen@kmslh.com>
2025-05-28 13:45:06 +0200

    Adding of linter

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended', 
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': ['error', { semi: false, singleQuote: true }],
+  },
+}
