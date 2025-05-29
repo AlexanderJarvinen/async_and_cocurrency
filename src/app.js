638b7ba52a7d32b47e19615ff96736abbcbf4a30
Aleksandr Yarvinen <Aleksandr.Yarvinen@kmslh.com>
2025-05-29 12:15:07 +0200

    Refactoring of addEventListener for all buttons

--- src/app.js
+++ src/app.js
@@ -15,47 +15,25 @@ import { initCharts } from './chartsUpdate.js'
 
 window.onload = () => {
   initCharts()
-  document
-    .getElementById('startMainChartDataLoadingButton')
-    .addEventListener('click', initData())
 
-  document
-    .getElementById('startWorkersButton')
-    .addEventListener('click', initData(true))
+  const platformInitMap = {
+    startMainChart: () => initData(),
+    startWorkers: () => initData(true),
+    youtube: initDataForYoutubeChart,
+    spotify: initDataForSpotifyChart,
+    instagram: initDataForInstaChart,
+    facebook: initDataForFacebookChart,
+    twitter: initDataForTwitterChart,
+    pandora: initDataForPandoraChart,
+    soundcloud: initDataForSoundcloudChart,
+    deezer: initDataForDeezerChart,
+    tiktok: initDataForTiktokChart,
+  }
 
-  document
-    .getElementById('youtubeDataLoadingButton')
-    .addEventListener('click', initDataForYoutubeChart)
-
-  document
-    .getElementById('spotifyDataLoadingButton')
-    .addEventListener('click', initDataForSpotifyChart)
-
-  document
-    .getElementById('instagramDataLoadingButton')
-    .addEventListener('click', initDataForInstaChart)
-
-  document
-    .getElementById('facebookDataLoadingButton')
-    .addEventListener('click', initDataForFacebookChart)
-
-  document
-    .getElementById('twitterDataLoadingButton')
-    .addEventListener('click', initDataForTwitterChart)
-
-  document
-    .getElementById('pandoraDataLoadingButton')
-    .addEventListener('click', initDataForPandoraChart)
-
-  document
-    .getElementById('soundcloudDataLoadingButton')
-    .addEventListener('click', initDataForSoundcloudChart)
-
-  document
-    .getElementById('deezerDataLoadingButton')
-    .addEventListener('click', initDataForDeezerChart)
-
-  document
-    .getElementById('tiktokDataLoadingButton')
-    .addEventListener('click', initDataForTiktokChart)
+  Object.entries(platformInitMap).forEach(([platform, handler]) => {
+    const button = document.getElementById(`${platform}DataLoadingButton`)
+    if (button) {
+      button.addEventListener('click', handler)
+    }
+  })
 }
